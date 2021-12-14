782f31bc81c999893a8bd003b8d45365be393dfc
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-12-15 02:48:04 +0600

    action added

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Book from '../components/Book/Book';
 // import books from '../fakeData/books.json';
 import PageLayout from '../components/PageLayout/PageLayout';
 import { useSelector } from 'react-redux';
+import { fetchBooks } from '../redux/slices/bookSlice';
+import { useDispatch } from 'react-redux';
+
 const Discover = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchBooks());
+    }, [])
+
     const books = useSelector((state) => state.books.discover);
     return (
         <PageLayout>
--- src/redux/slices/bookSlice.js
+++ src/redux/slices/bookSlice.js
@@ -1,13 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import books from '../../fakeData/books.json';
+
+// First, create the thunk
+export const fetchBooks = createAsyncThunk(
+    'book/fetchBooks',
+
+    async () => {
+        const response = await fetch('https://redux-book-shelf.herokuapp.com/books')
+            .then(res => res.json())
+        return response.data
+    }
+)
 
 const bookSlice = createSlice({
     name: 'book',
     initialState: {
-        discover: books,
+        discover: [],
         readingList: [],
         finishedList: []
     },
+
     reducers: {
         addToReadingList: (state, { payload }) => {
             state.readingList.push(payload)
@@ -15,6 +27,16 @@ const bookSlice = createSlice({
         removeFromReadingList: (state, { payload }) => {
             state.readingList = state.readingList.filter(book => book.id != payload.id)
         }
+    },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchBooks.fulfilled, (state, action) => {
+            state.discover = action.payload;
+            state.status = 'success';
+        })
+        builder.addCase(fetchBooks.pending, (state, action) => {
+            state.status = 'pending';
+        })
     },
 });
 
